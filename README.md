3c825b13fe032812c4a5e61eb4a445f5e32bdcb4
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2023-07-07 13:43:00 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ultrafast
+The game speed can be set to either Slow or Fast, but there is an inaccessible(?) third option for "Ultrafast". In most cases it behaves identical to Fast, but there's some situations in which ultrafast is coded to behave faster.
+
+## Config
+* Ultrafast Requires Fast - If true, ultrafast will only be enabled if the game speed is already set to fast. If false, then it will always be enabled.
+
+> Written with [StackEdit](https://stackedit.io/).
---
+++ UltrafastPlugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+namespace AtOUltrafast
+{
+    [BepInPlugin("com.DestroyedClone.Ultrafast", "Ultrafast", "0.1.0")]
+    public class AtOUltrafastPlugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> cfgCUltrafastRequiresFast;
+
+        public void Start()
+        {
+            cfgCUltrafastRequiresFast = Config.Bind("", "Ultrafast Requires Fast", true, "If true, ultrafast will only be enabled if the game speed is already set to fast. If false, then it will always be enabled.");
+
+            On.SettingsManager.LoadPrefs += SettingsManager_LoadPrefs;
+        }
+
+        private void SettingsManager_LoadPrefs(On.SettingsManager.orig_LoadPrefs orig, SettingsManager self)
+        {
+            orig(self);
+            if (!cfgCUltrafastRequiresFast.Value || GameManager.Instance.configGameSpeed == Enums.ConfigSpeed.Fast)
+            {
+                GameManager.Instance.configGameSpeed = Enums.ConfigSpeed.Ultrafast;
+            }
+        }
+    }
+}
